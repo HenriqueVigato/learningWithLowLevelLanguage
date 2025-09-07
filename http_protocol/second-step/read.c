1788d5d5fcac9887b150770b3ebf6d292052fddc
henrique vigato <henriquevigato@gmail.com>
2025-09-07 19:08:42 -0300

    Finalizado a segunda parte onde esta sendo lido '8bits' por vez e impresso uma linha inteira

--- http_protocol/second-step/read.c
+++ http_protocol/second-step/read.c
@@ -3,9 +3,19 @@
 #include <string.h>
 
 int printing_to_the_end(FILE *textPtr) {
+  char myString[180] = "";
   char myMessage[9];
   while (fgets(myMessage, 9, textPtr) != NULL) {
-    printf("%s\n", myMessage);
+    char *quebra = strchr(myMessage, '\n');
+
+    if (quebra != NULL) {
+      *quebra = '\0';
+      strcat(myString, myMessage);
+      printf("read: %s\n", myString);
+      myString[0] = '\0';
+    } else {
+      strcat(myString, myMessage);
+    }
   }
   return 0;
 }
@@ -17,11 +27,6 @@ int main() {
   if (txtPtr == NULL) {
     printf("Erro: %s\n", strerror(errno));
   }
-
-  // char myMessage[8];
-  // fgets(myMessage, 8, txtPtr);
-  //
-  // printf("%s\n", myMessage);
   printing_to_the_end(txtPtr);
 
   fclose(txtPtr);
