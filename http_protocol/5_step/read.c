1e04887e8b9632fd4525a4344beef643817e0de7
henrique vigato <henriquevigato@gmail.com>
2025-10-12 12:33:19 -0300

    Criando os arquivos necessarios para a quinta etapa

---
+++ http_protocol/5_step/read.c
@@ -0,0 +1,81 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int tcpClient(const char *addr, const int port) {
+  int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_fd < 0) {
+    perror("socket criation failed!");
+    return -1;
+  }
+
+  struct sockaddr_in server_addres;
+  server_addres.sin_family = AF_INET;
+  server_addres.sin_addr.s_addr = inet_addr(addr);
+  server_addres.sin_port = htons(port);
+
+  if (connect(client_fd, (struct sockaddr *)&server_addres,
+              sizeof(server_addres)) < 0) {
+    perror("Connection failed");
+    close(client_fd);
+    return -1;
+  } else {
+    printf("Connection succed! \n");
+  }
+
+  return client_fd;
+}
+
+void printData(int client) {
+  char *buffer = malloc(9 * sizeof(char));
+  while (1) {
+    // limpa o buffer para nao ter nada
+    memset(buffer, 0, 9 * sizeof(char));
+
+    int dataReceived = recv(client, buffer, (9 * sizeof(char)) - 1, 0);
+    if (dataReceived < 0) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        printf("\nBuffer do sistema vazio!\n");
+      } else {
+        perror("Erro no recv");
+        free(buffer);
+      }
+      break;
+    } else if (dataReceived == 0) {
+      free(buffer);
+      break;
+    }
+
+    char *newLine = strchr(buffer, '\n');
+    if (newLine != NULL) {
+      *newLine = '\0';
+      printf("%s", buffer);
+      printf("\n");
+      printf("read: %s", newLine + 1);
+      fflush(stdout);
+    } else {
+      printf("%s", buffer);
+      fflush(stdout);
+    }
+  }
+}
+
+int main() {
+  char addr[] = "127.0.0.1";
+  int port = 43000;
+
+  int clientConnection = tcpClient(addr, port);
+  printf("read: ");
+  fflush(stdout);
+  printData(clientConnection);
+
+  close(clientConnection);
+  printf("Conexao Fechada\n");
+
+  return 0;
+}
---
+++ http_protocol/5_step/tcpServer.c
@@ -0,0 +1,77 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+char *readFile(FILE *txtPtr) {
+
+  char *myString = malloc(1024 * sizeof(char));
+
+  if (fgets(myString, 1024, txtPtr) != NULL) {
+    return myString;
+  } else {
+    free(myString);
+    return NULL;
+  }
+}
+FILE *openFile(char *path) {
+  FILE *txtPtr;
+
+  txtPtr = fopen(path, "r");
+
+  if (txtPtr == NULL) {
+    printf("Erro: %s\n", strerror(errno));
+  }
+  return txtPtr;
+}
+
+int main(void) {
+  // TCP server
+  char ipAddress[] = "127.0.0.1";
+  int port = 43000;
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = inet_addr(ipAddress);
+  address.sin_port = htons(port);
+  int result = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+
+  if (result == 0) {
+    printf("✅ Bind realizado com sucesso!\n\n");
+  } else {
+    printf("❌ Erro no bind!\n\n");
+    perror("bind");
+    return -1;
+  }
+
+  listen(server_fd, 3);
+
+  printf("Server is running and listening on port %d...\n\n", port);
+  while (1) {
+    int addrlen = sizeof(address);
+    int new_socket =
+        accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (new_socket < 0) {
+      perror("Failed to accept connection\n");
+      continue;
+    }
+    printf("connection accept\n");
+
+    FILE *msg = openFile("./messages.txt");
+    char *menssagem = "";
+    while ((menssagem = readFile(msg)) != NULL) {
+      write(new_socket, menssagem, strlen(menssagem));
+    }
+    close(new_socket);
+    fclose(msg);
+    printf("connection close\n");
+  }
+
+  close(server_fd);
+  return 0;
+}
