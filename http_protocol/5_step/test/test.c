814bb0e5b8443d1c930ff6a130753d1dd8dfc997
henrique vigato <henriquevigato@gmail.com>
2025-10-12 21:29:49 -0300

    Fazendo testes para o meu programa

--- http_protocol/5_step/test/test.c
+++ http_protocol/5_step/test/test.c
@@ -1,7 +1,24 @@
-#include "unity.h"
+#include "../Unity/src/unity.h"
+#include "../read.h"
 
-int maid(void) {
+void setUp() {}
+void tearDown() {}
+
+void test_string(void) {
+  char str1[] = "Hello";
+  char str2[] = "Hello";
+  TEST_ASSERT_EQUAL_STRING(str1, str2);
+}
+
+void test_resd_test(void) {
+  int value = test();
+  TEST_ASSERT_EQUAL_INT(6, value);
+}
+
+int main(void) {
   UNITY_BEGIN();
-  RUN_TEST();
+  RUN_TEST(test_string);
+  RUN_TEST(test_resd_test);
+
   return UNITY_END();
 }
